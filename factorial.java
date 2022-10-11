5e7473baef7d3fa5ed88b6e8c186a26bce99bb90
Abduino <abdrehmanredi0@gmail.com>
2022-10-11 17:34:04 -0300

    java factorial example

---
+++ factorial.java
@@ -0,0 +1,15 @@
+public class fact
+{
+    public static void main (String[] args)
+ {
+
+        int factor = 1;
+       System.out.println("factorial of numbers b/n 1-10 are:-");
+
+        for (int i=1; i<=10; i++)
+{
+            factor = factor*i;
+
+         System.out.print("Factorial of " + i + ": ");
+         System.out.println(factor + " ");
+    }}}
